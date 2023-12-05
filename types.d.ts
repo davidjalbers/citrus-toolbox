2a780a395f428a001358388c36b6d31e35004a69
David Julian Albers <dev@davidjalbers.com>
2023-12-06 00:11:34 +0100

    refactor: unify declarations under types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -3,3 +3,11 @@
 // whether you're running in development or production).
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
 declare const MAIN_WINDOW_VITE_NAME: string;
+
+  interface ImportMeta {
+    env: {
+      VITE_DEFAULT_PRIVACY_FORM_FILE_PATH?: string;
+      VITE_DEFAULT_SURVEY_FILE_PATH?: string;
+      VITE_DEFAULT_OUTPUT_DIR_PATH?: string;
+    };
+  }
