f68bb278c22821342cdc40b218ef5c4c12be8ce6
David Julian Albers <dev@davidjalbers.com>
2023-12-06 00:41:12 +0100

    refactor: extract hardcoded sample values from job report

--- components/App.tsx
+++ components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -25,7 +25,6 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 
 
@@ -117,24 +116,42 @@ function JobForm() {
   );
 }
 
-const JobResultDialog = () => {
+type JobResultDialogProps = {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  resultData: {
+    timestamp: string,
+    totalStudyCodes: number,
+    totalEntries: number,
+    totalDuplicates: number,
+    valid: number,
+    noConsent: number,
+    onlyPrivacyForm: number,
+    onlySurvey: number,
+    invalid: number,
+    privacyFormFileName: string,
+    surveyFileName: string,
+    outputDirectoryName: string,
+  },
+};
+const JobResultDialog: React.FC<JobResultDialogProps> = ({ isOpen, setIsOpen, resultData }) => {
+  if (!resultData) return null;
   return (
-    <AlertDialog>
-      <AlertDialogTrigger>Open</AlertDialogTrigger>
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
       <AlertDialogContent className={cn("rounded-lg")}>
         <AlertDialogHeader>
-          <AlertDialogTitle>Job Execution Report</AlertDialogTitle>
+          <AlertDialogTitle>Job Execution Report ({resultData.timestamp})</AlertDialogTitle>
           <AlertDialogDescription className={cn("mb-2 font-bold")}>All operations completed successfully.</AlertDialogDescription>
-          <AlertDialogDescription className={cn("mb-2")}>Read input from <span className={cn("font-mono text-xs")}>`Datenschutz.csv`</span> and <span className={cn("font-mono text-xs")}>`Umfrage.csv`</span>.</AlertDialogDescription>
+          <AlertDialogDescription className={cn("mb-2")}>Read input from <span className={cn("font-mono text-xs")}>`{resultData.privacyFormFileName}`</span> and <span className={cn("font-mono text-xs")}>`{resultData.surveyFileName}`</span>.</AlertDialogDescription>
           <AlertDialogDescription className={cn("mb-2")}>
-            Processed a total of 511 study codes in 934 entries (102 duplicates):
-            <li>384 valid and usable</li>
-            <li>49 without consent to use of data</li>
-            <li>40 only in privacy form (no entries found in survey data)</li>
-            <li>10 only in survey (no entries found privacy form data)</li>
-            <li>28 invalid</li>
+            Processed a total of {resultData.totalStudyCodes} study codes in {resultData.totalEntries} entries ({resultData.totalDuplicates} duplicates):
+            <li>{resultData.valid} valid and usable</li>
+            <li>{resultData.noConsent} without consent to use of data</li>
+            <li>{resultData.onlyPrivacyForm} only in privacy form (no entries found in survey data)</li>
+            <li>{resultData.onlySurvey} only in survey (no entries found privacy form data)</li>
+            <li>{resultData.invalid} invalid</li>
           </AlertDialogDescription>
-        <AlertDialogDescription className={cn("mb-2")}>Output files written to <span className={cn("font-mono text-xs")}>`citrus-toolbox`</span>.</AlertDialogDescription>
+        <AlertDialogDescription className={cn("mb-2")}>Output files written to <span className={cn("font-mono text-xs")}>`{resultData.outputDirectoryName}`</span>.</AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Close</AlertDialogCancel>
@@ -146,11 +163,32 @@ const JobResultDialog = () => {
 };
 
 export const App = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [resultData, setResultData] = useState<JobResultDialogProps["resultData"]>(null);
+  const onSubmit = useCallback(async (data: JobForm) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setResultData({
+      timestamp: new Date().toISOString(),
+      totalStudyCodes: 511,
+      totalEntries: 934,
+      totalDuplicates: 102,
+      valid: 384,
+      invalid: 28,
+      noConsent: 49,
+      onlyPrivacyForm: 40,
+      onlySurvey: 10,
+      privacyFormFileName: 'Datenschutz.csv',
+      surveyFileName: 'Umfrage.csv',
+      outputDirectoryName: 'citrus-toolbox',
+    });
+    setIsDialogOpen(true);
+    console.log(data)
+  }, [setIsDialogOpen, setResultData]);
   return (
     <main className={cn("p-5")}>
       <h1 className={cn("font-black text-center text-4xl mb-10")}>Consent Matcher</h1>
       <JobForm />
-      <JobResultDialog />
+      <JobResultDialog isOpen={isDialogOpen} setIsOpen={setIsDialogOpen} resultData={resultData} />
     </main>
   );
 };
