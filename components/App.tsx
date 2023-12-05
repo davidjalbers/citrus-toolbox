ec3d85919a580bc571d26250cdc84293a40b42f0
David Julian Albers <dev@davidjalbers.com>
2023-12-06 00:28:07 +0100

    Revert "chore: remove orphaned import"
    This reverts commit 5ff606f72c7d378594a65741d71d444b452183d8.

--- components/App.tsx
+++ components/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { productName } from '@/package.json';
+
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -25,40 +27,30 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
-import { Loader2 } from 'lucide-react';
 
-const filePath = z.string()
-  .trim()
-  .min(1, 'This field is required.')
-  .refine((value) => electron.validatePath({ path: value }), 'This file either does not exist or is not readable.');
-const directoryPath = z.string()
-  .trim()
-  .min(1, 'This field is required.')
-  .refine((value) => electron.validatePath({ path: value, type: 'directory', access: 'readWrite' }), 'This directory either does not exist or is not writable.');
 
-const JobFormSchema = z.object({
-  privacyFormFilePath: filePath,
-  surveyFilePath: filePath,
-  outputDirectoryPath: directoryPath,
+const FormSchema = z.object({
+  privacyFormFilePath: z.string().refine((val) => val.length <= 10, {
+    message: "String can't be more than 10 characters",
+  }),
+  surveyFilePath: z.string(),
+  outputDirectoryPath: z.string(),
 });
-type JobForm = z.infer<typeof JobFormSchema>;
 
-type JobFormProps = {
-  onSubmit: (data: JobForm) => void | Promise<void>;
-};
-const JobForm: React.FC<JobFormProps> = ({ onSubmit }) => {
-  const form = useForm<JobForm>({
-    resolver: zodResolver(JobFormSchema),
+function JobForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
     defaultValues: {
       privacyFormFilePath: import.meta.env.VITE_DEFAULT_PRIVACY_FORM_FILE_PATH || '',
       surveyFilePath: import.meta.env.VITE_DEFAULT_SURVEY_FILE_PATH || '',
       outputDirectoryPath: import.meta.env.VITE_DEFAULT_OUTPUT_DIR_PATH || '',
     },
-    mode: 'onSubmit',
-    reValidateMode: 'onBlur',
   });
-  const isLoading = form.formState.isSubmitting;
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className={cn("flex flex-col space-y-10")}>
@@ -70,11 +62,8 @@ const JobForm: React.FC<JobFormProps> = ({ onSubmit }) => {
               <FormLabel className={cn("font-bold")}>Privacy Form File Path</FormLabel>
               <FormControl>
                 <div className={cn("flex gap-1")}>
-                  <Input {...field} spellCheck={false} placeholder='/path/to/privacy.csv'/>
-                  <Button type="button" onClick={async () => {
-                    const path = await electron.selectPath();
-                    if (path) form.setValue('privacyFormFilePath', path, { shouldValidate: true });
-                  }}>Select file</Button>
+                  <Input {...field} />
+                  <Button type="button">Select file</Button>
                 </div>
               </FormControl>
               <FormDescription>
@@ -90,13 +79,10 @@ const JobForm: React.FC<JobFormProps> = ({ onSubmit }) => {
           render={({ field }) => (
             <FormItem>
               <FormLabel className={cn("font-bold")}>Survey File Path</FormLabel>
-              <FormControl >
+              <FormControl>
                 <div className={cn("flex gap-1")}>
-                  <Input {...field} spellCheck={false} placeholder='/path/to/survey.csv'/>
-                  <Button type="button" onClick={async () => {
-                    const path = await electron.selectPath();
-                    if (path) form.setValue('surveyFilePath', path, { shouldValidate: true });
-                  }}>Select file</Button>
+                  <Input {...field} />
+                  <Button type="button">Select file</Button>
                 </div>
               </FormControl>
               <FormDescription>
@@ -115,11 +101,8 @@ const JobForm: React.FC<JobFormProps> = ({ onSubmit }) => {
               <FormLabel className={cn("font-bold")}>Output Directory Path</FormLabel>
               <FormControl>
                 <div className={cn("flex gap-1")}>
-                  <Input {...field} spellCheck={false} placeholder='/path/to/output' />
-                  <Button type="button" onClick={async () => {
-                    const path = await electron.selectPath({ type: 'directory' });
-                    if (path) form.setValue('outputDirectoryPath', path, { shouldValidate: true });
-                  }}>Select directory</Button>
+                  <Input {...field} />
+                  <Button type="button">Select directory</Button>
                 </div>
               </FormControl>
               <FormDescription>
@@ -130,63 +113,34 @@ const JobForm: React.FC<JobFormProps> = ({ onSubmit }) => {
           )}
         />
         <hr />
-        <Button type="submit" size="lg" disabled={isLoading}>
-          { isLoading ? (
-            <>
-              <Loader2 className={cn("animate-spin inline-block mr-2")} />
-              Working...
-            </>
-          ) : (
-            <>Run job</>
-          )}
-        </Button>
+        <Button type="submit" size="lg">Run job</Button>
       </form>
     </Form>
   );
 }
 
-type JobResultDialogProps = {
-  isOpen: boolean;
-  setIsOpen: (isOpen: boolean) => void;
-  resultData: {
-    timestamp: string,
-    totalStudyCodes: number,
-    totalEntries: number,
-    totalDuplicates: number,
-    valid: number,
-    noConsent: number,
-    onlyPrivacyForm: number,
-    onlySurvey: number,
-    invalid: number,
-    privacyFormFileName: string,
-    surveyFileName: string,
-    outputDirectoryName: string,
-  },
-};
-const JobResultDialog: React.FC<JobResultDialogProps> = ({ isOpen, setIsOpen, resultData }) => {
-  if (!resultData) return null;
+const JobResultDialog = () => {
   return (
-    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+    <AlertDialog>
+      <AlertDialogTrigger>Open</AlertDialogTrigger>
       <AlertDialogContent className={cn("rounded-lg")}>
         <AlertDialogHeader>
-          <AlertDialogTitle>Job Execution Report ({resultData.timestamp})</AlertDialogTitle>
+          <AlertDialogTitle>Job Execution Report</AlertDialogTitle>
           <AlertDialogDescription className={cn("mb-2 font-bold")}>All operations completed successfully.</AlertDialogDescription>
-          <AlertDialogDescription className={cn("mb-2")}>Read input from <span className={cn("font-mono text-xs")}>`{resultData.privacyFormFileName}`</span> and <span className={cn("font-mono text-xs")}>`{resultData.surveyFileName}`</span>.</AlertDialogDescription>
+          <AlertDialogDescription className={cn("mb-2")}>Read input from <span className={cn("font-mono text-xs")}>`Datenschutz.csv`</span> and <span className={cn("font-mono text-xs")}>`Umfrage.csv`</span>.</AlertDialogDescription>
           <AlertDialogDescription className={cn("mb-2")}>
-            Processed a total of {resultData.totalStudyCodes} study codes in {resultData.totalEntries} entries ({resultData.totalDuplicates} duplicates):
-            <li>{resultData.valid} valid and usable</li>
-            <li>{resultData.noConsent} without consent to use of data</li>
-            <li>{resultData.onlyPrivacyForm} only in privacy form (no entries found in survey data)</li>
-            <li>{resultData.onlySurvey} only in survey (no entries found privacy form data)</li>
-            <li>{resultData.invalid} invalid</li>
+            Processed a total of 511 study codes in 934 entries (102 duplicates):
+            <li>384 valid and usable</li>
+            <li>49 without consent to use of data</li>
+            <li>40 only in privacy form (no entries found in survey data)</li>
+            <li>10 only in survey (no entries found privacy form data)</li>
+            <li>28 invalid</li>
           </AlertDialogDescription>
-        <AlertDialogDescription className={cn("mb-2")}>Output files written to <span className={cn("font-mono text-xs")}>`{resultData.outputDirectoryName}`</span>.</AlertDialogDescription>
+        <AlertDialogDescription className={cn("mb-2")}>Output files written to <span className={cn("font-mono text-xs")}>`citrus-toolbox`</span>.</AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Close</AlertDialogCancel>
-          <AlertDialogAction onClick={() => {
-            navigator.clipboard.writeText(JSON.stringify(resultData, null, 2));
-          }}>Copy to clipboard</AlertDialogAction>
+          <AlertDialogAction>Copy to clipboard</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
@@ -194,32 +148,11 @@ const JobResultDialog: React.FC<JobResultDialogProps> = ({ isOpen, setIsOpen, re
 };
 
 export const App = () => {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [resultData, setResultData] = useState<JobResultDialogProps["resultData"]>(null);
-  const onSubmit = useCallback(async (data: JobForm) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    setResultData({
-      timestamp: new Date().toISOString(),
-      totalStudyCodes: 511,
-      totalEntries: 934,
-      totalDuplicates: 102,
-      valid: 384,
-      invalid: 28,
-      noConsent: 49,
-      onlyPrivacyForm: 40,
-      onlySurvey: 10,
-      privacyFormFileName: 'Datenschutz.csv',
-      surveyFileName: 'Umfrage.csv',
-      outputDirectoryName: 'citrus-toolbox',
-    });
-    setIsDialogOpen(true);
-    console.log(data)
-  }, [setIsDialogOpen, setResultData]);
   return (
     <main className={cn("p-5")}>
       <h1 className={cn("font-black text-center text-4xl mb-10")}>Consent Matcher</h1>
-      <JobForm onSubmit={onSubmit} />
-      <JobResultDialog isOpen={isDialogOpen} setIsOpen={setIsDialogOpen} resultData={resultData} />
+      <JobForm />
+      <JobResultDialog />
     </main>
   );
 };
