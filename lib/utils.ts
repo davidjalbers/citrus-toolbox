bc588f4776527a68934f1600ee1a648cabdc674f
David Julian Albers <dev@davidjalbers.com>
2023-12-06 19:16:20 +0100

    feat: add toUpperSnake utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function toUpperSnake(str: string) {
+  return str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1_$2').toUpperCase();
+}
