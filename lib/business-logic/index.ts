27f725af72e03590676d7176c909f9b47c03ef5d
David Julian Albers <dev@davidjalbers.com>
2024-03-11 16:50:37 +0100

    feat: add debug output

--- lib/business-logic/index.ts
+++ lib/business-logic/index.ts
@@ -3,6 +3,8 @@ import { createJobArgFromCsv, getHeadersFromCsv, writeJobResultToCsv } from './c
 import { JobResultStats, executeJob } from './core';
 import { IOSelection } from '@/components/ps-matcher/IOSelectionForm';
 import { HeaderSelection } from '@/components/ps-matcher/HeaderSelectionForm';
+import fs from 'fs';
+import path from 'path';
 
 export async function processInputSelectionImpl(info: IOSelection): Promise<Headers> {
   await new Promise(resolve => setTimeout(resolve, 500));
@@ -35,6 +37,14 @@ export async function processColumnDefinitionAndRunJobImpl(
     surveyFilePath: info.surveyFilePath,
     outputDirectoryPath: info.outputDirectoryPath,
   });
+
+  // Write the entire result object as JSON
+  // to the output directory for debugging purposes
+  fs.writeFileSync(path.join(info.outputDirectoryPath, 'job_result_object.json'), JSON.stringify(result, null, 2));
+  fs.writeFileSync(
+    path.join(info.outputDirectoryPath, 'job_result_object_no_passthrough.json'),
+    JSON.stringify(result, (key, value) => (key === 'passthrough' ? undefined : value), 2),
+  );
 
   return result.stats;
 }
