ee4f20897b763a53154d14de4b716f380071f854
David Julian Albers <dev@davidjalbers.com>
2024-03-12 01:21:36 +0100

    chore: remove unused import

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, dialog, shell, Menu } from 'electron';
+import { app, BrowserWindow, ipcMain, dialog, shell } from 'electron';
 import path from 'path';
 import * as fs from 'fs/promises';
 
